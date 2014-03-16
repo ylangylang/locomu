0650ac71613ebfe804a1b08c431980005e1f6275
sufica <ylang365@gmail.com>
2014-03-17 07:54:58 +0900

    smileアイコンを押下した際に親画面のリボンのカウンタを更新する処理を実装中。
    user_photos.jsの処理が書きかけ

---
+++ app/assets/javascripts/user_photos.js
@@ -0,0 +1,9 @@
+$(function() {
+    $('.button').click(function () {
+        $('#smile_ribbon-area').addClass("selected");
+    });
+
+    $('#user_photos_index_modal').on('hidden.bs.modal', function () {
+        // do something...
+    })
+});
